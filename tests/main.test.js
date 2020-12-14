efac57188d5ec8be702cc55c9e7563d0ca4ff421
Louis Thirifay <louis.thirifay@gmail.com>
2020-12-14 21:47:44 +0100

    test créer compte et se connecter fonctionnent

--- tests/main.test.js
+++ tests/main.test.js
@@ -8,27 +8,32 @@ const url = 'https://localhost:8080';
 
 const capabilities = Capabilities.chrome();
 capabilities.set(Capability.ACCEPT_INSECURE_TLS_CERTS, true);
+capabilities.setPageLoadStrategy("normal");
+
+jest.setTimeout(30000);
 
 describe('tests to create an account / login', () => {
     //let driver = new Builder().forBrowser("chrome").build();
-    let driver = new Builder()
-        .withCapabilities(capabilities)
-        .forBrowser('chrome')
-        .build();
+    let driver;
     let dbo;
+    
 
     beforeAll(async () => {
+        driver = new Builder()
+                .withCapabilities(capabilities)
+                .forBrowser('chrome')
+                .build();
         connection = await MongoClient.connect('mongodb://localhost:27017', {
             useNewUrlParser: true,
             useUnifiedTopology: true
         });
-        dbo = await connection.db("testdb");
-        await dbo.collection('users').drop();
+        dbo = await connection.db("database");
+        await dbo.collection('users').remove();
     },15000);
 
     beforeEach(async () => {
         driver.manage().deleteAllCookies();
-    }, 5000);
+    }, 15000);
 
     afterAll(async() => {
         await connection.close();
@@ -38,21 +43,31 @@ describe('tests to create an account / login', () => {
 
     test('Create an account and check the page we get back has Groupes in its title', async () => {
         await driver.get(url);
+        await driver.manage().window().maximize();
         await driver.findElement(By.id('username')).sendKeys('Xx_Killer_xX');
         await driver.findElement(By.id('pwd')).sendKeys('Hello123');
         await driver.findElement(By.id('name')).sendKeys('Jacques Dupont');
         await driver.findElement(By.id('mail')).sendKeys('Jacques.Dupont@gmail.com');
-        await driver.findElement(By.id('submitLogIn')).click();
+        await driver.wait(until.elementLocated(By.id('submitRegister')),10000);
+        let button = await driver.findElement(By.id('submitRegister'));
+        await driver.wait(until.elementIsEnabled(button,15000));
+        await driver.executeScript("arguments[0].click();", button);
         let title = await driver.getTitle();
+        console.log('first');
         expect(title).toContain('Groupes');
     });
 
     test('Login with correct password and check the page we get back has Groupes in its title', async () => {
         await driver.get(url);
+        await driver.manage().window().maximize();
         await driver.findElement(By.id('usernamealready')).sendKeys('Xx_Killer_xX');
         await driver.findElement(By.id('pwdalready')).sendKeys('Hello123');
-        await driver.findElement(By.id('submitRegister')).click();
+        await driver.wait(until.elementLocated(By.id('submitLogIn')),10000);
+        let button = await driver.findElement(By.id('submitLogIn'));
+        await driver.wait(until.elementIsEnabled(button,15000));
+        await driver.executeScript("arguments[0].click();", button);
         let title = await driver.getTitle();
+        console.log('second');
         expect(title).toContain('Groupes');
     });
 
